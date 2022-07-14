1274b3d269c28adeb00acc8471ecfc4147793775
Gabriel Faustino <gabrielfa.andrade@gmail.com>
2022-07-14 19:10:23 -0300

    Requisito 10 parcial

---
+++ src/components/SearchBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+function SearchBar() {
+  return (
+    <div>
+      <input type="text" data-testid="search-input" />
+      <div>
+        {/* <input type="radio" id="ingredient" name="ingredient" value="ingredient"
+              checked>
+        <label for="huey">Huey</label> */}
+      </div>
+    </div>
+  );
+}
+
+export default SearchBar;
