d247dff4394d6a43bb946c9ea1524dba3671f1b9
Gabriel Faustino <gabrielfa.andrade@gmail.com>
2022-07-14 15:59:33 -0300

    Requisito 4 sem teste

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,19 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
+  const [login, setLogin] = useState({
+    email: '',
+    senha: '',
+  });
+
+  const validateEmail = () => {
+    const { email } = login;
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  };
+  const validatePassword = () => {
+    const { senha } = login;
+    const NUM = 6;
+    const teste = senha.length > NUM;
+    return teste;
+  };
+
+  const handleChange = ({ id, value }) => {
+    setLogin((prev) => ({
+      ...prev,
+      [id]: value,
+    }));
+  };
+
   return (
     <div>
       <fieldset>
         <legend>Login</legend>
         <label htmlFor="email">
           Email:
-          <input type="email" id="email" data-testid="email-input" />
+          <input
+            type="email"
+            id="email"
+            data-testid="email-input"
+            onChange={ (event) => handleChange(event.target) }
+          />
         </label>
         <label htmlFor="senha">
           Senha:
-          <input type="password" id="senha" data-testid="password-input" />
+          <input
+            type="password"
+            id="senha"
+            data-testid="password-input"
+            onChange={ (event) => handleChange(event.target) }
+          />
         </label>
-        <button type="button" data-testid="login-submit-btn">
+        <button
+          type="button"
+          disabled={ !(validateEmail() && validatePassword()) }
+          data-testid="login-submit-btn"
+        >
           Enter
         </button>
       </fieldset>
