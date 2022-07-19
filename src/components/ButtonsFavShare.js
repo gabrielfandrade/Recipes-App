1a6e7b66c0d63a307c7d61ea941f27049e77b34f
Gabriel Faustino <gabrielfa.andrade@gmail.com>
2022-07-19 18:07:44 -0300

    Clipboard

--- src/components/ButtonsFavShare.js
+++ src/components/ButtonsFavShare.js
@@ -1,24 +1,25 @@
 import React /* { useState } */ from 'react';
 import PropTypes from 'prop-types';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
+import copy from 'clipboard-copy';
 import shareIcon from '../images/shareIcon.svg';
 
 function ButtonsFavShare({ copyUrl }) {
-  // console.log(copyUrl);
   return (
     <div>
       <button type="button" data-testid="favorite-btn">
         Favorite
       </button>
 
-      <CopyToClipboard text={ copyUrl }>
-        <button type="button" data-testid="share-btn">
-          <img
-            src={ shareIcon }
-            alt="share-url"
-          />
-        </button>
-      </CopyToClipboard>
+      <button
+        type="button"
+        data-testid="share-btn"
+        onClick={ () => copy(copyUrl) }
+      >
+        <img
+          src={ shareIcon }
+          alt="share-url"
+        />
+      </button>
     </div>
   );
 }
